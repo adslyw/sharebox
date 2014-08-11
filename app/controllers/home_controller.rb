f2be6bc19b60974dda498c7a605a2a8fe5c06be8
adslyw <adslyw@gmail.com>
2014-08-11 11:08:00 +0800

    viewing a folder shared by others and download the files from the sharedfolders

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,26 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
+      #show folders shared by others
+      @being_shared_folders = current_user.shared_folders_by_others
       @folders = current_user.folders.roots
       @assets = current_user.assets.where("folder_id is NULL").order("uploaded_file_file_name desc")
     end
   end
   def browse
     @current_folder = current_user.folders.find(params[:folder_id])
+    @is_this_folder_being_shared = false if @current_folder #just an instance variable to help hiding buttons on View
+
+    #if not found in own folders, find it in being_shared_folders
+    if @current_folder.nil?
+      folder = Folder.find_by_id(params[:folder_id])
+
+      @current_folder ||= folder if current_user.has_share_access?(folder)
+      @is_this_folder_being_shared = true if @current_folder #just an instance variable to help hiding buttons on View
+
+    end
     if @current_folder
+      @being_shared_folders = []
       @folders = @current_folder.children
       @assets = @current_folder.assets.order("uploaded_file_file_name desc")
       render :action => "index"
@@ -24,9 +37,10 @@ class HomeController < ApplicationController
       @shared_folder.shared_email = email_address
 
       shared_user = User.find_by_email(email_address)
+      @shared_folder.shared_user_id =shared_user.id if shared_user
       @shared_folder.message = params[:message]
       @shared_folder.save
-      UserMailer.invitation_to_share(@shared_folder).deliver
+      #UserMailer.invitation_to_share(@shared_folder).deliver
       respond_to do |format|
        format.js
       end
